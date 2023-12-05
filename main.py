165af45e52afbda104cc36e77cca3fab34d78597
sweetfranklin <sweetfranklin99@gmail.com>
2023-12-05 22:45:45 +0800

    2023-12-05 22:45:45

--- main.py
+++ main.py
@@ -55,3 +55,4 @@ bool ParserAdapter::initExt(const char* id, IParserApi* api)
 		{
 			ContractSet contractSet;
 			WTSArray* ayContract = _bd_mgr->getContracts();
+			WTSArray::Iterator it = ayContract->begin();
