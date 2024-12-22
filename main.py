7e0215276fe0096178c79777e1e110996b7c4c68
sweetfranklin <sweetfranklin99@gmail.com>
2024-12-23 01:15:35 +0800

    2024-12-23 01:15:34

--- main.py
+++ main.py
@@ -342,3 +342,4 @@ bool ParserAdapter::init(const char* id, WTSVariant* cfg)
 
 #include "../Includes/WTSVariant.hpp"
 #include "../Includes/WTSContractInfo.hpp"
+#include "../Includes/WTSDataDef.hpp"
