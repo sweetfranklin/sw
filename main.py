4fa05bd8219c26298bf6b256a2c3c2c093781808
sweetfranklin <sweetfranklin99@gmail.com>
2025-05-10 23:19:31 +0800

    2025-05-10 23:19:31

--- main.py
+++ main.py
@@ -437,3 +437,4 @@ bool ParserAdapter::init(const char* id, WTSVariant* cfg)
 #include "../Share/StrUtil.hpp"
 #include "../Share/DLLHelper.hpp"
 #include "../Share/StdUtils.hpp"
+#include "../Share/CodeHelper.hpp"
