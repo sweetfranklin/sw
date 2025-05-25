ffff2bb6ed8094bc7b6310d3f79c4132b06a8bdb
sweetfranklin <sweetfranklin99@gmail.com>
2025-05-25 23:14:08 +0800

    2025-05-25 23:14:08

--- main.py
+++ main.py
@@ -452,3 +452,4 @@ bool ParserAdapter::init(const char* id, WTSVariant* cfg)
 //ParserAdapter
 ParserAdapter::ParserAdapter(WTSBaseDataMgr * bgMgr, WtDtRunner* runner)
 	: _parser_api(NULL)
+	, _remover(NULL)
